e1ff6eb9865b8336d2eb9d8475dfcbbf64edd4d9
Basil Brule <basilb34@live.fr>
2025-12-16 14:47:13 +0100

    Ajout routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ListeProjet } from './pages/liste-projet/liste-projet';
+import { DetailProjet } from './pages/detail-projet/detail-projet';
+import { AjouterProjet } from './pages/ajouter-projet/ajouter-projet';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: ListeProjet},
+    { path: 'projet/:id', component: DetailProjet },
+    { path: 'ajouter-projet', component: AjouterProjet },
+    {path: '**', redirectTo: '' } /* pour secuisé : ** redirige tout URL non defini vers '' (1er path)*/
+    
+];
