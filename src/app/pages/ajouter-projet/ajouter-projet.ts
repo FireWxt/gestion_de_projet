5a641f00036dcb05a61e5e2bcd48b0ce1e3185ba
Simon Daconceicao Semiao <semiaosimon@gmail.com>
2025-12-16 14:11:55 +0100

    modification structure

---
+++ src/app/pages/ajouter-projet/ajouter-projet.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ajouter-projet',
+  imports: [],
+  templateUrl: './ajouter-projet.html',
+  styleUrl: './ajouter-projet.scss',
+})
+export class AjouterProjet {
+
+}
