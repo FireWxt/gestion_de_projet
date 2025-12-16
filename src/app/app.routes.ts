00cf3689c1d27a2438af687373c32547be5e4308
Simon Daconceicao Semiao <semiaosimon@gmail.com>
2025-12-16 14:39:33 +0100

    Ajout du service pour liste projet et details projet, et prise en charge d'ajouterProjet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'ajouter-projet', loadComponent: () => import('./pages/ajouter-projet/ajouter-projet').then(m => m.AjouterProjet) },
+];
--- src/app/services/projet.services.ts
+++ src/app/services/projet.services.ts
@@ -1,4 +1,4 @@
-  import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 export interface Projet {
   id: number;
@@ -11,22 +11,21 @@ export interface Projet {
 })
 export class ProjetService {
 
-    
+
   private projets: Projet[] = [
-    { id: 1, nom: 'Site e-commerce', statut: 'En cours' },
-    { id: 2, nom: 'Application mobile', statut: 'Terminé' }
+    { id: 1, nom: 'Projet Angular', statut: 'En cours' },
+    { id: 2, nom: 'Projet Java', statut: 'Terminé' }
   ];
 
   constructor() {}
 
 
-  getProjets(): Projet[] {
+  getListeProjets(): Projet[] {
     return this.projets;
   }
 
-
-  getProjetById(id: number): Projet | undefined {
-    return this.projets.find(p => p.id === id);
+  getDetailProjet(id: number): Projet | undefined {
+    return this.projets.find(projet => projet.id === id);
   }
 
   ajouterProjet(nom: string, statut: string): void {
